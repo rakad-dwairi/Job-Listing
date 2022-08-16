314d11a08de2ea5c916e4200e22ed1967a4618ab
RakadDwairi8 <rakad@newaeon-development.com>
2022-08-16 16:08:28 +0300

    Lated Update

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -14,7 +14,23 @@
           <a href="{{ URL::to('about') }}" class="nav-item nav-link">About</a>
           <a href="{{ URL::to('Contact-Us') }}" class="nav-item nav-link">Contact</a>
       </div>
-      <a href="{{ URL::to('Dashboard') }}" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Profile<i class="fa fa-arrow-right ms-3"></i></a>
+      {{-- <a href="{{ URL::to('Dashboard') }}" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Profile<i class="fa fa-arrow-right ms-3"></i></a> --}}
+      <div class="dropdown">
+        <button class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fa fa-user fa-2x"></i>
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="{{ URL::to('Dashboard') }}"> <i class="fa fa-tachometer" aria-hidden="true"></i> DashBoard</a>
+          <a class="dropdown-item" href="#"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a>
+          <a href="#" class="nav-link"
+                    onclick="event.preventDefault(); document.getElementById('logoutform').submit();">
+                    <i class="nav-icon fas fa-fw fa-sign-out-alt">
+
+                    </i>
+                    {{ trans('global.logout') }}
+                </a>
+        </div>
+      </div>
   </div>
 </nav>
 </header>
